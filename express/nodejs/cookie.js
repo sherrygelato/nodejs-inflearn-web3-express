f12c1e4b34fe9d753e181a3b1b7c8a32139b58ce
sherrygelato <sherrygelato@kakao.com>
2021-12-06 17:39:05 +0900

    쿠키와 인증 6. session vs permanent

--- express/nodejs/cookie.js
+++ express/nodejs/cookie.js
@@ -3,8 +3,11 @@ var cookie = require('cookie')
 
 // 처음 Set-Cookie 했을 때 응답 헤더 => 주석처리 하니까 요청 헤더에 쿠키
 http.createServer(function (request, response) {
+
     console.log(request.headers.cookie)
+
     var cookies = {}
+
     if (request.headers.cookie !== undefined) {
         cookies = cookie.parse(request.headers.cookie)
     }
@@ -14,7 +17,16 @@ http.createServer(function (request, response) {
     console.log(`tasty_cookie: ${cookies.tasty_cookie}`)
     
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco','tasty_cookie=strawberry']
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookiess; Max-Age=${60*60*24*30}` // 30일
+        ]
+        // 오래 지속되는 쿠키 만들기 
+        // Expires (절대적) 쿠키 만료
+        // or Max- Age(현시점부터)쿠키 얼마동안 살 것인가 (초단위)
     })
+
     response.end('Cookie!!')
+
 }).listen(3000)
