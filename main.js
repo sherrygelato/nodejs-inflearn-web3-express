2628b15493a42258fa3d61aa141fa9ac363e339c
sherrygelato <sherrygelato@kakao.com>
2021-12-06 10:58:20 +0900

    5. Route Path n Url

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const port = 3000
 
 var fs = require('fs')
 var template = require('./lib/template.js')
+var path = require('path')
+var sanitizeHtml = require('sanitize-html')
 
 /*
 route, routing
@@ -26,7 +28,27 @@ app.get('/', (request, response) => {
 })
 
 app.get('/page/:pageID', (request, response) => {
-  response.send(request.params)
+  fs.readdir('./data', function (error, filelist) {
+    var filteredId = path.parse(request.params.pageID).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageID;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 })
 
 // app.listen(3000);
